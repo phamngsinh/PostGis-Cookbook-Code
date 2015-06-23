b26b1961918b453b9549ce93ce10d80861e894fd
smagic39 <smagic39@gmail.com>
2015-06-23 07:21:23 +0700

    eee

--- Chapter 3/import_gpx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in `find runkeeper_gpx -name \*.gpx -printf "%f\n"`
-do
-    echo "Importing gpx file $f to chp03.rk_track_points PostGIS table..." #, ${f%.*}"
-    ogr2ogr -append -update  -f PostgreSQL PG:"dbname='postgis_cookbook' user='me' password='mypassword'" runkeeper_gpx/$f -nln chp03.rk_track_points -sql "SELECT ele, time FROM track_points"
-done
